015cd775d681fc8fd09a6283a34bb597bebc1a41
Aman Maheshwari <amanmaheshwari715@gmail.com>
2025-03-22 15:24:03 +0530

    Updated path

--- next.config.mjs
+++ next.config.mjs
@@ -12,8 +12,8 @@ const isGithubActor = process.env.NEXT_PUBLIC_GITHUB_ACTOR === "upsidedownlabs" 
 const config = {
   reactStrictMode: true,
   output: "export", // Ensures it works with static export
-  basePath: isGithubActor ? "" : "/Muscle_game",
-  assetPrefix: isGithubActor ? "" : "/Muscle_game",
+  basePath:"",
+  assetPrefix: "",
   images: {
     unoptimized: true,
     remotePatterns: [
